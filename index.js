3a666a2077422d791ca4d6a80f9c6eed9c10fd93
matperez01 <144254480+matperez01@users.noreply.github.com>
2024-04-30 16:28:11 -0300

    creacionAPI

---
+++ index.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import fs from 'fs';
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+    try {
+        const data = fs.readFileSync("./db.json");
+        return JSON.parse(data);
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
+}
+
+const writeData = (data) => {
+    try {
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get("/", (req, res) => {
+    res.send("bienvenido a mi primer API!")
+});
+
+app.get("/books", (req, res) => {
+    const data = readData();
+    res.json(data.books || []);
+});
+
+app.get("/books/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const book = data.books.find((book) => book.id === id);
+    res.json(book);
+});
+
+app.post("/books", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const newBook = {
+        id: data.books ? data.books.length + 1 : 1,
+        ...body,
+    };
+    data.books = data.books || [];
+    data.books.push(newBook);
+    writeData(data);
+    res.json(newBook);
+});
+
+app.put("/books/:id", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books[bookIndex] = {
+        ...data.books[bookIndex],
+        ...body,
+    };
+    writeData(data);
+    res.json({ message: "Book updated successfully" });
+});
+
+app.delete("/books/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books.splice(bookIndex, 1);
+    writeData(data);
+    res.json({ message: "Book deleted successfully" });
+  });
+
+  
+app.listen(3001, () => {
+    console.log('Server listening on port 3001');
+});
+
